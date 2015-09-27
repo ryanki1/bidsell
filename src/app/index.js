d428ab3ef0c0a3db518a2bbeda737badff38a38f
Kieran Ryan <kebten@googlemail.com>
2015-09-27 14:10:23 +0200

    Header refactored the angular-material way and form given a new purple theme :-)

--- src/app/index.js
+++ src/app/index.js
@@ -6,7 +6,15 @@ import quotePriceGenerator from '../app/quote/quotePriceGenerator.js';
 angular.module('git', ['ngSanitize', 'ui.router', 'ngMaterial'])
   .controller('BidSellCtrl', BidSellCtrl)
   .factory('quotePriceGenerator', quotePriceGenerator)
-
+  .config(function($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+      .primaryPalette('purple', {
+        'default': '100',
+        'hue-1': '200',
+        'hue-2': '300',
+        'hue-3': '500'
+      })
+  })
   .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
